4c89ac5893227d02ea60aeeb079992417e114d0b
= <=>
2024-10-21 23:45:49 -0600

    aplicando tailwind para el login y dandole formato al navbar del dashboard de admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,43 +1,65 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        
-    <nav>
-        <ul>
-            @if(auth()->user()->role === 'admin')
-                <li><a href="#">Admins</a></li>
-                <li><a href="{{ route('admin.index') }}">Teachers</a></li>
-                <li><a href="{{ route('admin.students') }}">Students</a></li>
-            @elseif(auth()->user()->role === 'teacher')
-                <li><a href="{{ route('teacher.dashboard') }}">Dashboard</a></li>
-            @elseif(auth()->user()->role === 'student')
-                <li><a href="#">Dashboard</a></li>
-            @endif
-            <li><a href="{{ route('logout') }}" 
-                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        @if (auth()->user()->role === 'admin')
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Admin</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+                    aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarScroll">
+                    <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Admins</a>
+                        </li>
+                        <li class="nav-item"><a class="nav-link active" href="{{ route('admin.index') }}">Teachers</a>
+                        </li>
+                        <li class="nav-item"><a class="nav-link active"
+                                href="{{ route('admin.students') }}">Students</a></li>
+                    @elseif(auth()->user()->role === 'teacher')
+                        <li class="nav-item"><a class="nav-link" href="{{ route('teacher.dashboard') }}">Dashboard</a></li>
+                        @elseif(auth()->user()->role === 'student')
+                        <li class="nav-item"><a class="nav-link" href="#">Dashboard</a></li>
+        @endif
+        <li class="nav-item"><a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
         </ul>
+        <form class="d-flex" id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+        </div>
+        </div>
     </nav>
 
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        @csrf
-    </form>
+
 
     <div class="container">
         @yield('content')
     </div>
-    </body>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+</script>
+
 </html>
